3d37896c287c8c2b0bd10f706cf03e675e423dc6
Promise <glelud@gmail.com>
2020-02-02 22:25:03 +0100

    Core features added to the bot

---
+++ app.js
@@ -0,0 +1,77 @@
+const Discord = require("discord.js"), fs = require("fs"), config = require("./config.json"), constants = require("./constants")(config), { getPermissionLevel, scanLinks, emojis, emojiSnowflakes, linkRegex } = constants;
+
+const client = new Discord.Client({ messageSweepInterval: 60, messageCacheLifetime: 5, disableEveryone: true, disabledEvents: ["TYPING_START", "PRESENCE_UPDATE", "GUILD_BAN_ADD", "GUILD_BAN_REMOVE", "USER_NOTE_UPDATE", "USER_SETTINGS_UPDATE", "VOICE_STATE_UPDATE", "VOICE_SERVER_UPDATE", "RELATIONSHIP_ADD", "RELATIONSHIP_REMOVE"], restTimeOffset: 200, fetchAllMembers: true })
+const db = require("./database.js")(client, config, constants);
+
+client.on("ready", () => {
+  console.log("Ready as " + client.user.tag);
+})
+
+const commands = {}, aliases = {} // { "command": require("that_command") }, { "alias": "command" }
+fs.readdir("./commands/", (err, files) => {
+  if (err) console.error(err);
+  for (let file of files) if (file.endsWith(".js")) {
+    let commandFile = require("./commands/" + file), fileName = file.replace(".js", "")
+    commands[fileName] = commandFile
+    if (commandFile.aliases) for (let alias of commandFile.aliases) aliases[alias] = fileName
+  }
+})
+
+client.on("message", async message => {
+  if (message.author.bot || !message.guild) return;
+
+  const links = message.content.match(linkRegex); // https://stackoverflow.com/a/3809435
+  if (getPermissionLevel(message.member) < 1) {
+    const results = await scanLinks(links);
+    if (!results.safe) {
+      return;
+    }
+  }
+
+  if (message.content.startsWith(config.prefix) || message.content.match(`^<@!?${client.user.id}> `)) {
+    if (!message.member && message.author.id) try { message.member = await message.guild.fetchMember(message.author.id, true) } catch(e) {} // If a member is missing, we try to load that member in before executing the command.
+
+    let args = message.content.split(" ");
+    if (args[0].match(`^<@!?${client.user.id}>`)) args.shift(); else args = message.content.slice(config.prefix.length).split(" ");
+    const identifier = args.shift().toLowerCase(), command = aliases[identifier] || identifier
+
+    const commandFile = commands[command], permissionLevel = getPermissionLevel(message.member)
+    if (commandFile) {
+      if (permissionLevel < commandFile.permissionRequired) return message.channel.send("❌ You don't have permission to do this!");
+      if (commandFile.checkArgs(args, permissionLevel) !== true) return message.channel.send("❌ Invalid arguments! Usage is `" + config.prefix + command + Object.keys(commandFile.usage).map(a => " " + a).join("") + "\`, for additional help type `" + config.prefix + "help " + command + "`.");
+      
+      commandFile.run(client, message, args, config, constants, permissionLevel, db)
+    }
+  } else if (message.content.match(`^<@!?${client.user.id}>`)) return message.author.send({
+    embed: {
+      title: client.user.tag,
+      description: [
+        emojis.wave + " I'm a moderation bot designed for Project Blurple.",
+        "📖 My source code can be located here: [https://github.com/...](https://github.com/project-blurple/blurple-hammer)",
+        "📄 My documentation can be found here: [https://docs-for.a-mod-bot.but-it-actually.works](https://docs-for.a-mod-bot.but-it-actually.works)"
+      ].join("\n"),
+      color: 7506394
+    }
+  }).then(() => message.react(emojiSnowflakes.thumbsup)).catch(() => message.react(emojiSnowflakes.thumbsdown))
+})
+
+client
+  .on("messageUpdate", async (message, newMessage) => {
+    if (!message.guild || message.author.bot) return;
+
+    let oldLinks = message.content.match(linkRegex), newLinks = newMessage.content.match(linkRegex), diff = newLinks ? newLinks.filter(l => !oldLinks.includes(l)) : []
+    if (getPermissionLevel(message.member) < 1) {
+      const results = await scanLinks(diff);
+      if (!results.safe) {
+        return;
+      }
+    }
+  })
+
+  .on("rateLimit", rl => rl.path.includes("reactions") ? null : console.log("Rate limited. [" + rl.timeDifference + "ms, endpoint: " + rl.path + ", limit: " + rl.limit + "]"))
+  .on("disconnect", dc => console.log("Disconnected:", dc))
+  .on("reconnecting", () => console.log("Reconnecting..."))
+  .on("resume", replayed => console.log("Resumed. [" + replayed + " events replayed]"))
+  .on("error", err => console.log("Unexpected error:", err))
+  .on("warn", warn => console.log("Unexpected warning:", warn))
+  .login(config.token)
---
+++ constants/index.js
@@ -0,0 +1,106 @@
+const fs = require("fs");
+
+module.exports = config => ({
+  embedColor: 0x7289DA,
+  hexColor: "7289DA",
+  getPermissionLevel: member => {
+    let roles = member.roles.map(r => r.id)
+    
+    if (config.owner === member.user.id) return 7; // Promise#0001
+    if (roles.includes("443013283977494539")) return 6; // admin
+    if (roles.includes("413213839866462220")) return 5; // exec
+    if (roles.includes("470272155876065280")) return 4; // sr.mod
+    if (roles.includes("442690537691021333")) return 3; // mod
+    if (roles.includes("562886834301042698")) return 2;
+    if (roles.includes("442785212502507551")) return 1; // helper
+    return 0; // normal user
+  },
+  scanLinks: require("./link-scanner.js"),
+  emojis: {
+    loading: '<a:loading:572202235342225418>',
+    blurple: '<:blurple:673265867840290859>',
+    thumbsup: '<:thumbsup:673265868301533194>',
+    birthdayhat: '<:birthdayhat:673265867995217931>',
+    tickyes: '<:tickyes:673265868213452855>',
+    drippingheart: '<:drippingheart:673265868318310401>',
+    pingsock: '<:pingsock:673265868385550376>',
+    hammer: '<:hammer:673265868414910520>',
+    darkblurple: '<:darkblurple:673265868444270602>',
+    tada: '<:tada:673265868188287007>',
+    tickno: '<:tickno:673265868461047828>',
+    star: '<:star:673265868490145825>',
+    wave: '<:wave:673265868498796591>',
+    weewoo: '<a:weewoo:673265868079366155>',
+    sparkle: '<:sparkle:673265868603654183>',
+    love: '<a:love:673265868666437632>',
+    white: '<:white:673265868716638208>',
+    thumbsdown: '<:thumbsdown:673265868662112269>',
+    heart: '<:heart:673265871094939674>'
+  },
+  emojiSnowflakes: {
+    loading: '572202235342225418',
+    blurple: '673265867840290859',
+    thumbsup: '673265868301533194',
+    birthdayhat: '673265867995217931',
+    tickyes: '673265868213452855',
+    drippingheart: '673265868318310401',
+    pingsock: '673265868385550376',
+    hammer: '673265868414910520',
+    darkblurple: '673265868444270602',
+    tada: '673265868188287007',
+    tickno: '673265868461047828',
+    star: '673265868490145825',
+    wave: '673265868498796591',
+    weewoo: '673265868079366155',
+    sparkle: '673265868603654183',
+    love: '673265868666437632',
+    white: '673265868716638208',
+    thumbsdown: '673265868662112269',
+    heart: '673265871094939674' 
+  },
+  linkRegex: /[-a-zA-Z0-9@:%._\+~#=]{2,}\.[a-zA-Z0-9()]{2,24}\b([-a-zA-Z0-9()@:%_\+.~#?&\/\/=]*)/gm,
+  urlBlacklist: fs.readFileSync("./constants/url-blacklist.txt", "utf8").split("\n").map(l => l.replace("\r", "")),
+  urlWhitelist: fs.readFileSync("./constants/url-whitelist.txt", "utf8").split("\n").map(l => l.replace("\r", "")),
+  linkCategories: {
+    // NEGATIVE
+    101: 'MALWARE_OR_VIRUS',
+    102: 'POOR_CUSTOMER_EXPERIENCE',
+    103: 'PHISHING',
+    104: 'SCAM',
+    105: 'POTENTIALLY_ILLEGAL',
+  
+    // QUESTIONABLE
+    201: 'MISLEADING_CLAIMS_OR_UNETHICAL',
+    202: 'PRIVACY_RISKS',
+    203: 'SUSPICIOUS',
+    204: 'HATE_DISCRIMINTATION',
+    205: 'SPAM',
+    206: 'PUP',
+    207: 'ADS_POPUPS',
+  
+    // NEUTRAL
+    301: 'ONLINE_TRACKING',
+    302: 'ALTERNATIVE_OR_CONTROVERSIAL_NATURE',
+    303: 'OPINIONS_RELIGION_POLITICS',
+    304: 'OTHER',
+  
+    // CHILD_SAFETY
+    401: 'ADULT_CONTENT',
+    402: 'INCIDENTAL_NUDITY',
+    403: 'GRUESOM_OR_SHOCKING',
+    404: 'SITE_FOR_KIDS',
+  
+    // POSITIVE
+    501: 'GOOD_SITE',
+  
+    Meta: {
+      NEGATIVE: 100,
+      QUESTIONABLE: 200,
+      NEURTAL: 300,
+      CHILD_SAFETY: 400,
+      POSITIVE: 500,
+    },
+  },
+  rules: require("./rules.json"),
+  staffgl: fs.existsSync("./constants/staffgl.json") ? require("./staffgl.json") : {}
+})
