f727efbcb7c778be316b51e6363d54554369522f
Promise <glelud@gmail.com>
2020-02-05 20:57:35 +0100

    Lock command updating command requirements

--- commands/lock.js
+++ commands/lock.js
@@ -3,8 +3,12 @@ module.exports = {
   usage: {},
   examples: {},
   aliases: [],
-  checkArgs: (args) => args.length == 1
   permissionRequired: 2, // 0 All, 1 Helper, 2 JR.Mod, 3 Mod, 4 SR.Mod, 5 Exec, 6 Admin, 7 Promise#0001
+  checkArgs: (args) => {
+    if (args[0] == "-all" && args.length == 1) return true;
+    else if (!args.length) return true;
+    else return false;
+  }
 }
 
 module.exports.run = module.exports.run = async (client, message, args, config, constants, permissionLevel, db) => {}
