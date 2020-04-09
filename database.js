4a0413b0fd7aeffb4901289a9dc76ee2f3b5cef3
Promise <glelud@gmail.com>
2020-04-09 18:47:50 +0200

    Remove old code, rewriting

--- database.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-module.exports = (client, config, constants) => {
-  mongoose.connect(encodeURI(config.database_uri), { useNewUrlParser: true, useUnifiedTopology: true });
-
-  return {
-    
-  }
-}
