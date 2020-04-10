205e219b48b746f1fd81c742b523b44fc9e25d7d
Promise <glelud@gmail.com>
2020-04-11 01:37:35 +0200

    Simple help command

---
+++ src/commands/help.js
@@ -0,0 +1,22 @@
+module.exports = {
+  description: "Get help on how to use the bot.",
+  usage: {},
+  examples: {},
+  aliases: [],
+  permissionRequired: 0, // 0 All, 1 Helper, 2 JR.Mod, 3 Mod, 4 SR.Mod, 5 Exec, 6 Admin, 7 Promise#0001
+  checkArgs: (args) => !args.length
+}
+
+module.exports.run = async (client, message, args, db, constants, { content }) => {
+  return message.author.send({
+    embed: {
+      title: client.user.tag,
+      description: [
+        `${constants.emojis.wave} I'm a moderation bot designed for Project Blurple.`,
+        "📖 My source code can be located here: [https://github.com/project-blurple/blurple-hammer](https://github.com/project-blurple/blurple-hammer)",
+        "📄 Although we do have documentation for the bot, it's personalized for Project Blurple's Staff team and also therefore not available to the public."
+      ].join("\n"),
+      color: constants.embedColor
+    }
+  }).then(() => message.react(constants.emojiSnowflakes.tickyes)).catch(() => message.react(constants.emojiSnowflakes.tickno))
+}
