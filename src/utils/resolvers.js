903733b1db27b0576939fd23b976a2f99062339c
Promise <glelud@gmail.com>
2020-04-11 16:55:27 +0200

    Add way to search via ID

--- src/utils/resolvers.js
+++ src/utils/resolvers.js
@@ -1,20 +1,22 @@
-const idResolver = /[0-9]+/, constants = require("../constants")
+const constants = require("../constants")
 
 module.exports.getRole = (search, guild) =>
   guild.roles.cache.find(r => r.name == search) ||
-  guild.roles.cache.find(r => r.name.toLowerCase() == search.toLowerCase())
+  guild.roles.cache.find(r => r.name.toLowerCase() == search.toLowerCase()) ||
+  guild.roles.cache.get(getId(search))
 
 module.exports.getMember = (search, guild) =>
   guild.members.cache.find(m => search == m.user.tag) ||
   guild.members.cache.find(m => search.toLowerCase() == m.user.tag.toLowerCase()) ||
   guild.members.cache.find(m => search == m.displayName) ||
-  guild.members.cache.find(m => search.toLowerCase() == m.displayName.toLowerCase())
+  guild.members.cache.find(m => search.toLowerCase() == m.displayName.toLowerCase()) ||
+  guild.members.cache.get(getId(search))
 
 module.exports.getChannel = (search, guild) => {
   const channels = guild.channels.cache.filter(ch => ch.type == "text" && ch.viewable);
   return false ||
     channels.find(ch => search.toLowerCase() == ch.name.toLowerCase()) ||
-    channels.get(search.match(idResolver))
+    channels.get(getId(search))
 }
 
 module.exports.getMembers = (searches, guild) => {
@@ -30,3 +32,15 @@ module.exports.getMembers = (searches, guild) => {
 
   return members.map(m => m.id).filter(constants.onlyUnique).map(id => members.find(m => m.id == id));
 }
+
+module.exports.getUser = async (search, guild) => {
+  const member = module.exports.getMember(search, guild);
+  if (member) return member.user;
+  else try {
+    return await guild.client.users.fetch(search)
+  } catch(e) {
+    return null;
+  }
+}
+
+const getId = search => (search.match(/[0-9]+/) || [null])[0]
