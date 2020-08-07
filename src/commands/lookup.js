29604137bd0060c9d227b17fdb24c0288580a2ce
Promise <glelud@gmail.com>
2020-08-07 16:49:45 +0200

    Typo

--- src/commands/lookup.js
+++ src/commands/lookup.js
@@ -5,7 +5,7 @@ module.exports = {
   },
   examples: {},
   aliases: [ "bot-lookup", "id-lookup", "invite-lookup", "whatis", "wit", "whatisthis" ],
-  permissionLevel: 1, // 0 All, 1 Helper, 2 JR.Mod, 3 Mod, 4 SR.Mod, 5 Exec, 6 Admin, 7 Promise#0001
+  permissionRequired: 1, // 0 All, 1 Helper, 2 JR.Mod, 3 Mod, 4 SR.Mod, 5 Exec, 6 Admin, 7 Promise#0001
   checkArgs: (args) => args.length == 1
 }
 
