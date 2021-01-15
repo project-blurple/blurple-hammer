9db56be8c1949f0ff04dcea80b8852dbf2830fe2
Robster <15860096+Gigabitzz@users.noreply.github.com>
2021-01-15 14:53:05 +0800

    Update toggleduty permissionRequired to 3

--- src/commands/toggleduty.js
+++ src/commands/toggleduty.js
@@ -5,7 +5,7 @@ module.exports = {
   },
   examples: {},
   aliases: [ "sod" ],
-  permissionRequired: 1, // 0 All, 1 Helper, 2 JR.Mod, 3 Mod, 4 SR.Mod, 5 Exec, 6 Admin, 7 Promise#0001
+  permissionRequired: 3, // 0 All, 1 Helper, 2 JR.Mod, 3 Mod, 4 SR.Mod, 5 Exec, 6 Admin, 7 Promise#0001
   checkArgs: (args) => true
 }
 
