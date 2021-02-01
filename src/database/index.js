f8766913212fcab065bb0fba22b83670350728bf
promise <glelud@gmail.com>
2021-02-01 11:43:49 -0800

    Add timezone command

--- src/database/index.js
+++ src/database/index.js
@@ -3,7 +3,7 @@ const database = require("quick-store");
 module.exports = {};
 
 for (const db of [
-  "strips", "domainstates", "oauth", "subserveraccessoverrides"
+  "strips", "domainstates", "oauth", "subserveraccessoverrides", "timezones"
 ]) {
   const qsdb = database(`${__dirname}/${db}.json`);
   module.exports[db] = {
