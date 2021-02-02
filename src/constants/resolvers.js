83c10db3505bf372e1c71ee228435ab4dbd19094
Promise <glelud@gmail.com>
2021-02-02 21:48:13 +0100

    Support user mentions and role mentions

--- src/constants/resolvers.js
+++ src/constants/resolvers.js
@@ -5,13 +5,13 @@ const { idResolver } = require("./regex");
 module.exports.getRole = (search, guild) =>
   guild.roles.cache.find(r => r.name == search) ||
   guild.roles.cache.find(r => r.name.toLowerCase() == search.toLowerCase()) ||
-  guild.roles.resolve(search);
+  guild.roles.resolve((search.match(idResolver) || ["0"])[0]);
 
 module.exports.getMember = (search, guild) =>
   guild.members.cache.find(m => search.toLowerCase() == m.user.tag.toLowerCase()) ||
   guild.members.cache.find(m => search == m.displayName) ||
   guild.members.cache.find(m => search.toLowerCase() == m.displayName.toLowerCase()) ||
-  guild.members.resolve(search);
+  guild.members.resolve((search.match(idResolver) || ["0"])[0]);
 
 module.exports.getChannel = (search, guild) =>
   guild.channels.cache.find(ch => search.toLowerCase() == ch.name.toLowerCase()) ||
