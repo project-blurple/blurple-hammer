c9eb6c8b9e05441c2544ddc37173f554c7cbfd31
flammableassassin <43076116+flamableassassin@users.noreply.github.com>
2021-02-25 16:54:46 +0000

    :art: Cleaning up things

--- src/commands/eval.js
+++ src/commands/eval.js
@@ -1,15 +1,11 @@
-
-
 module.exports = {
   description: "Evaluate some code.",
-  options: [
-    {
-      type: 3,
-      name: "code",
-      description: "The code you want to run through the bot.",
-      required: true
-    }
-  ],
+  options: [{
+    type: 3,
+    name: "code",
+    description: "The code you want to run through the bot.",
+    required: true
+  }],
   aliases: [],
   permissionRequired: 7 // 0 All, 1 Assistant, 2 Helper, 3 Moderator, 4 Exec.Assistant, 5 Executive, 6 Director, 7 Promise#0001
 };
@@ -18,10 +14,10 @@ module.exports.run = async ({ channel, client }, { code }) => {
   try {
     let evaled = await eval(code);
     if (typeof evaled != "string") evaled = require("util").inspect(evaled);
-    evaled = evaled.replace(client.token, "-- Redacted ---")
+    evaled = evaled.replace(client.token, "-- Redacted ---");
 
     channel.send(`🆗 Evaluated successfully.\n\`\`\`js\n${evaled}\`\`\``);
-  } catch(e) {
+  } catch (e) {
     let err;
     if (typeof e == "string") err = e.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
     else err = e;
