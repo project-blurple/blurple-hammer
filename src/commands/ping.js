d4440e94a6548248532d00f0143dbce2d879c11d
Glenn <glelud@gmail.com>
2021-04-29 09:36:05 +0200

    Merge branch '2021-security-update' into slash-commands-reworked

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -1,13 +1,11 @@
+const { emojis, functions: { msToTime } } = require("../constants");
+
 module.exports = {
   description: "Get the latency of the bot.",
-  usage: {},
-  examples: {},
   aliases: [ "pong", "latency", "uptime" ],
   permissionRequired: 0, // 0 All, 1 Assistant, 2 Helper, 3 Moderator, 4 Exec.Assistant, 5 Executive, 6 Director, 7 Promise#0001
   checkArgs: (args) => !args.length
 };
-
-const { emojis, functions: { msToTime } } = require("../constants");
 
 module.exports.run = async ({ client, channel }) => {
   const start = Date.now(), botMsg = await channel.send(`${emojis.loading} Pinging...`);
