c21d18ca35a51f15c5cc3d91ef47704abd49313c
Promise <glelud@gmail.com>
2021-05-06 01:50:26 +0200

    Bean command created, Proximity bugged me about it non-stop in DMs smh

---
+++ src/commands/slash/bean.js
@@ -0,0 +1,16 @@
+const { emojis } = require("../../constants");
+
+module.exports = {
+  description: "Bena someone",
+  options: [
+    {
+      type: 6,
+      name: "user",
+      description: "The user you want to bean.",
+      required: true
+    }
+  ],
+  permissionRequired: 1 // 0 All, 1 Assistant, 2 Helper, 3 Moderator, 4 Exec.Assistant, 5 Executive, 6 Director, 7 Promise#0001
+};
+
+module.exports.run = ({ respond }, { user }) => respond(`<@!${user}>, you've been beaned!`)
