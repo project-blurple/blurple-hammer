2490dca42ed2b4c68a57fd49a761c8a1cea48835
Promise <glelud@gmail.com>
2021-05-09 14:57:03 +0200

    Remove automatic restart at it will just loop

--- src/handlers/musicBotLegacyHandler.js
+++ src/handlers/musicBotLegacyHandler.js
@@ -4,11 +4,13 @@ const bot = "837825364540915762";
 
 module.exports = async client => {
   client.on("voiceStateUpdate", async (_, voice) => {
-    if (voice.member.id == bot) {
-      if (voice.channel && voice.channel.id == channels.musicManagingVoice) {
-        await client.api.guilds(guilds.main).members(bot).patch({ data: { channel_id: channels.eventsVoice } });
-        client.api.guilds(guilds.main)["voice-states"](bot).patch({ data: { channel_id: channels.eventsVoice, suppress: false } }); 
-      } else if (!voice.channel && config.restartKaraokeShellCommand) require("child_process").exec(config.restartKaraokeShellCommand);
+    if (
+      voice.member.id == bot &&
+      voice.channel &&
+      voice.channel.id == channels.musicManagingVoice
+    ) {
+      await client.api.guilds(guilds.main).members(bot).patch({ data: { channel_id: channels.eventsVoice } });
+      client.api.guilds(guilds.main)["voice-states"](bot).patch({ data: { channel_id: channels.eventsVoice, suppress: false } }); 
     }
   });
 };
