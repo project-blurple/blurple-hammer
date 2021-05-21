6c2449ed04ed07d4e5102d541e22dc433aec1a0e
Stijn van der Kolk <stijn@kolkies.dev>
2021-05-21 22:48:54 +0200

    fix: oopsie

--- src/commands/slash/strip.js
+++ src/commands/slash/strip.js
@@ -16,7 +16,7 @@ module.exports.run = async ({ client, guild, member: author, respond }, { user =
   const strip = await strips.get(user), member = guild.members.cache.get(user), executor = guild.members.cache.get(author);
   if (strip) {
     strips.unset(user);
-    member.roles.add(strip, user == author.id ? "User unstripped" : `${author.user.tag} (${author.user.id}) stripped`);
+    member.roles.add(strip, user == author.id ? "User unstripped" : `${author.user.tag} (${author.user.id}) unstripped`);
     return respond(`${emojis.tickyes} ${member.id == author.id ? "You are" : `${member.user.tag} is`} no longer stripped.`, true);
   } else if (getPermissionLevel({ id: executor.id, client }) < 1) return respond(`You cannot strip.`, true);
   else if (getPermissionLevel({ id: member.id, client }) >= 1) {
