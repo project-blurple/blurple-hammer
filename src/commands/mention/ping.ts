ff4b105a2bbc45ad6d02f1506bfd50c904697faf
Glenn <me@promise.solutions>
2022-10-24 07:13:27 +0000

    Rename time function

--- src/commands/mention/ping.ts
+++ src/commands/mention/ping.ts
@@ -1,6 +1,6 @@
 import Emojis from "../../constants/emojis";
 import type { MentionCommand } from ".";
-import { msToTime } from "../../utils/time";
+import { msToHumanShortTime } from "../../utils/time";
 
 const command: MentionCommand = {
   aliases: ["pong", ""],
@@ -8,7 +8,7 @@ const command: MentionCommand = {
   async execute(message, reply) {
     const now = Date.now();
     const botMessage = await reply(`${Emojis.Loading} Pinging...`);
-    return botMessage.edit(`${Emojis.Sparkle} Server latency is \`${Date.now() - now}ms\`, API latency is \`${Math.round(message.client.ws.ping)}ms\` and my uptime is \`${msToTime(message.client.uptime)}\`.`);
+    return botMessage.edit(`${Emojis.Sparkle} Server latency is \`${Date.now() - now}ms\`, API latency is \`${Math.round(message.client.ws.ping)}ms\` and my uptime is \`${msToHumanShortTime(message.client.uptime)}\`.`);
   },
 };
 
--- src/utils/time.ts
+++ src/utils/time.ts
@@ -1,18 +1,17 @@
 // https://stackoverflow.com/a/19700358
-export const msToTime = (ms: number): string => {
+export function msToHumanShortTime(ms: number): string {
   const days = Math.floor(ms / 86400000);
-  const daysms = ms % 86400000;
-  const hours = Math.floor(daysms / 3600000);
-  const hoursms = ms % 3600000;
-  const minutes = Math.floor(hoursms / 60000);
-  const minutesms = ms % 60000;
-  const sec = Math.floor(minutesms / 1000);
+  const daysMs = ms % 86400000;
+  const hours = Math.floor(daysMs / 3600000);
+  const hoursMs = daysMs % 3600000;
+  const minutes = Math.floor(hoursMs / 60000);
+  const minutesMs = hoursMs % 60000;
+  const seconds = Math.floor(minutesMs / 1000);
 
   let str = "";
   if (days) str += `${days}d`;
   if (hours) str += `${hours}h`;
   if (minutes) str += `${minutes}m`;
-  if (sec) str += `${sec}s`;
-
+  if (seconds) str += `${seconds}s`;
   return str || "0s";
-};
+}
