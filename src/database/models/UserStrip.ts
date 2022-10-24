55661673705ce933ab2d4f23f4aeff5a51366257
Glenn <me@promise.solutions>
2022-10-24 19:57:24 +0000

    Fix user strips database model

--- src/database/models/UserStrip.ts
+++ src/database/models/UserStrip.ts
@@ -1,10 +1,10 @@
-import { getModelForClass, prop } from "@typegoose/typegoose";
+import { PropType, getModelForClass, prop } from "@typegoose/typegoose";
 import type { DocumentType } from "@typegoose/typegoose";
 import type { Snowflake } from "discord.js";
 
 export class UserStripSchema {
   @prop({ type: String, required: true }) userId!: Snowflake;
-  @prop({ type: [String], required: true }) roleIds!: Snowflake[];
+  @prop({ type: [String], required: true }, PropType.ARRAY) roleIds!: Snowflake[];
 }
 
 export type UserStripDocument = DocumentType<UserStripSchema>;
