fbd35995b6dacc2a74a2292082eb34576cf3b57d
Glenn <me@promise.solutions>
2022-10-24 08:53:46 +0000

    Add restriction handlers

--- src/handlers/restrictions/nickname.ts
+++ src/handlers/restrictions/nickname.ts
@@ -1,3 +1,14 @@
+import { AuditLogEvent } from "discord.js";
 import type { Client } from "discord.js";
+import config from "../../config";
 
-export default function handleNicknameRestriction(client: Client<true>): void {}
+export default function handleNicknameRestriction(client: Client<true>): void {
+  client.on("guildMemberUpdate", async (old, member) => {
+    if (old.nickname !== member.nickname && member.roles.cache.has(config.roles.restrictions.nick)) {
+      const audits = await member.guild.fetchAuditLogs({ type: AuditLogEvent.MemberUpdate });
+      const entry = audits.entries.sort((a, b) => b.createdTimestamp - a.createdTimestamp).find(({ target, changes }) => target?.id === member.id && changes.some(change => change.key === "nick"));
+
+      if (entry?.executor?.id === member.id) await member.setNickname(old.nickname, "User is restricted from changing their nickname");
+    }
+  });
+}
--- src/handlers/restrictions/reactions.ts
+++ src/handlers/restrictions/reactions.ts
@@ -1,3 +1,9 @@
 import type { Client } from "discord.js";
+import config from "../../config";
 
-export default function handleReactionsRestriction(client: Client<true>): void {}
+export default function handleReactionsRestriction(client: Client<true>): void {
+  client.on("messageReactionAdd", async (reaction, user) => {
+    const member = await reaction.message.guild?.members.fetch({ user: user.id, force: false }).catch(() => null);
+    if (member?.roles.cache.has(config.roles.restrictions.reactions)) await reaction.users.remove(user.id);
+  });
+}
