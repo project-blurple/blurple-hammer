ce8b84049b92503937c6faf647762998b853cfa0
Promise <promise@projectblurple.com>
2023-02-13 08:16:28 +0000

    chore: remove unused type
    Signed-off-by: Promise <promise@projectblurple.com>

--- src/constants/autocompletes/index.ts
+++
@@ -1,5 +0,0 @@
-import type { ApplicationCommandOptionChoiceData, AutocompleteInteraction, Awaitable } from "discord.js";
-
-export interface Autocomplete<QueryType extends boolean | number | string> {
-  execute(query: QueryType, interaction: AutocompleteInteraction<"cached">): Awaitable<ApplicationCommandOptionChoiceData[]>;
-}
