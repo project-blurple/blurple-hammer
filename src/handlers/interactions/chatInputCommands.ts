51aea8892f7aaf1e904caaeabac3b95af3c88c71
Promise <promise@projectblurple.com>
2023-03-08 12:50:47 +0000

    fix(commands): non-public commands no longer locked to owner
    Signed-off-by: Promise <promise@projectblurple.com>

--- src/handlers/interactions/chatInputCommands.ts
+++ src/handlers/interactions/chatInputCommands.ts
@@ -1,6 +1,5 @@
 import type{ ChatInputCommandInteraction } from "discord.js";
 import { allChatInputCommands } from "../../commands/chatInput";
-import config from "../../config";
 
 export default function chatInputCommandHandler(interaction: ChatInputCommandInteraction<"cached">): void {
   const hierarchy = [interaction.commandName, interaction.options.getSubcommandGroup(false), interaction.options.getSubcommand(false)] as const;
@@ -8,8 +7,5 @@ export default function chatInputCommandHandler(interaction: ChatInputCommandInt
   if (command && hierarchy[1] && "subcommands" in command) command = command.subcommands.find(({ name }) => name === hierarchy[1]);
   if (command && hierarchy[2] && "subcommands" in command) command = command.subcommands.find(({ name }) => name === hierarchy[2]);
 
-  if (command && "execute" in command) {
-    if (!command.public && interaction.user.id !== config.ownerId) return;
-    return void command.execute(interaction);
-  }
+  if (command && "execute" in command) return void command.execute(interaction);
 }
