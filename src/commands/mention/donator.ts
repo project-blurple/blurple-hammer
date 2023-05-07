9b505e6eb65f1a9e502b0d32f6eb62947ff80c5b
Promise <promise@projectblurple.com>
2023-05-07 15:37:44 +0000

    feat: add quick donator role command
    Signed-off-by: Promise <promise@projectblurple.com>

---
+++ src/commands/mention/donator.ts
@@ -0,0 +1,24 @@
+import config from "../../config";
+import Emojis from "../../constants/emojis";
+import type { MentionCommand } from ".";
+
+export default {
+  names: ["d"],
+  testArgs(args) { return args.length === 1 || args.length === 2 && args[1] === "m"; },
+  async execute(_, reply, [userId, mega]) {
+    // only active in staff server
+    if (_.guildId !== "559341262302347314") return;
+
+    const member = await _.client.guilds.cache.get(config.mainGuildId)?.members.fetch(userId!);
+    if (!member) return void reply(`${Emojis.TickNo} Member not found on main server`);
+
+    const role = mega ? config.roles.megaDonators : config.roles.donators;
+    if (member.roles.cache.has(role)) {
+      void member.roles.remove(role, "Donator role removed");
+      return void reply(`${Emojis.TickYes} ${mega ? "Mega donator" : "Donator"} role removed from ${member.user.tag}.`);
+    }
+
+    void member.roles.add(role, "Donator role added");
+    return void reply(`${Emojis.TickYes} ${mega ? "Mega donator" : "Donator"} role added to ${member.user.tag}.`);
+  },
+} as MentionCommand;
