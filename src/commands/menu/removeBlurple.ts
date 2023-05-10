2a53f5da12a2ed4294a66113769c536a264eac2c
TheCodingGuy <officialtcgmatt@gmail.com>
2023-05-10 04:36:40 -0400

    feat(commands): add 'remove blurple role' user command (#316)
    * Add removeBlurple
    
    * fix(lint): fix lint
    
    Signed-off-by: Promise <promise@projectblurple.com>
    
    * fix(lint): fix formatting
    
    Signed-off-by: Promise <promise@projectblurple.com>
    
    ---------
    
    Signed-off-by: Promise <promise@projectblurple.com>
    Co-authored-by: Promise <promise@projectblurple.com>

---
+++ src/commands/menu/removeBlurple.ts
@@ -0,0 +1,16 @@
+import config from "../../config";
+import Emojis from "../../constants/emojis";
+import type { MenuCommand } from ".";
+
+export default {
+  name: "Remove Blurple User",
+  type: "user",
+  execute(interaction, target) {
+    if (target.roles.cache.has(config.roles.blurpleUser)) {
+      void target.roles.remove(config.roles.blurpleUser);
+      return void interaction.reply({ content: `${Emojis.TickYes} ${target.user.tag} no longer has the Blurple User role.`, ephemeral: true });
+    }
+
+    return void interaction.reply({ content: `${Emojis.TickNo} ${target.user.tag} does not have the Blurple User role.`, ephemeral: true });
+  },
+} as MenuCommand;
