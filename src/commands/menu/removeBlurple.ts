707dfd9227b4e1bbe030e1e33b5a47aa4d9ed29e
Promise <promise@projectblurple.com>
2023-05-10 08:35:07 +0000

    fix(lint): fix formatting
    Signed-off-by: Promise <promise@projectblurple.com>

--- src/commands/menu/removeBlurple.ts
+++ src/commands/menu/removeBlurple.ts
@@ -6,12 +6,11 @@ export default {
   name: "Remove Blurple User",
   type: "user",
   execute(interaction, target) {
-
     if (target.roles.cache.has(config.roles.blurpleUser)) {
       void target.roles.remove(config.roles.blurpleUser);
       return void interaction.reply({ content: `${Emojis.TickYes} ${target.user.tag} no longer has the Blurple User role.`, ephemeral: true });
     }
-    return void interaction.reply({ content: `${Emojis.TickNo} ${target.user.tag} does not have the Blurple User role.`, ephemeral: true });
 
+    return void interaction.reply({ content: `${Emojis.TickNo} ${target.user.tag} does not have the Blurple User role.`, ephemeral: true });
   },
 } as MenuCommand;
