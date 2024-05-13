a5bc07687e6d4b4a475604b546b990f3b16cedbb
Promise <promise@projectblurple.com>
2024-05-13 13:16:57 +0000

    fix: lint
    Signed-off-by: Promise <promise@projectblurple.com>

--- src/handlers/restrictions/reactions.ts
+++ src/handlers/restrictions/reactions.ts
@@ -2,8 +2,8 @@ import type { Client } from "discord.js";
 import config from "../../config";
 
 export default function handleReactionsRestriction(client: Client<true>): void {
-  client.on("messageReactionAdd", async (reaction, user) => {
+  client.on("messageReactionAdd", (reaction, user) => void (async () => {
     const member = await reaction.message.guild?.members.fetch({ user: user.id, force: false }).catch(() => null);
     if (member?.roles.cache.has(config.roles.restrictions.reactions)) await reaction.users.remove(user.id);
-  });
+  })());
 }
